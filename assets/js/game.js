fd67c93a63bf90fa4c4115792d7a9e886fb0a3c2
Rowan Gibbard <rowangibbard01@gmail.com>
2024-11-28 13:11:38 +0000

    fix bug by removing redirect to end.html

--- assets/js/game.js
+++ assets/js/game.js
@@ -215,7 +215,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS ) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html') 
+       // return window.location.assign('/end.html') - this has been commented out as without including an end.html, previews will return a 404
     }
     questionCounter++ 
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}` 
